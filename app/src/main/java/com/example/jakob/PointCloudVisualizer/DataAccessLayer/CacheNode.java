fe0d894608c0e004d5e770cfef5bd57138cb48a9
Jakob Krause <ulukaiz@hotmail.com>
2015-12-01 05:13:51 +0100

    fix buffer initalization

--- app/src/main/java/com/example/jakob/PointCloudVisualizer/DataAccessLayer/CacheNode.java
+++ app/src/main/java/com/example/jakob/PointCloudVisualizer/DataAccessLayer/CacheNode.java
@@ -4,16 +4,17 @@ package com.example.jakob.PointCloudVisualizer.DataAccessLayer;
 import java.nio.FloatBuffer;
 
 public class CacheNode {
-        int key;
+        String key;
         int offset;
         CacheNode pre;
         CacheNode next;
 
-        public CacheNode(int key, float[] data, int offset, FloatBuffer vertexBuffer,
+        public CacheNode(String key, float[] vertices, float[] colors, int offset, FloatBuffer vertexBuffer,
                          FloatBuffer colorBuffer){
             this.key = key;
             this.offset= offset;
             vertexBuffer.position(offset);
-            vertexBuffer.put(data);
+            vertexBuffer.put(vertices);
+            colorBuffer.put(colors);
         }
 }
--- app/src/main/java/com/example/jakob/PointCloudVisualizer/DataAccessLayer/LRUCache.java
+++ app/src/main/java/com/example/jakob/PointCloudVisualizer/DataAccessLayer/LRUCache.java
@@ -1,54 +1,81 @@
 package com.example.jakob.PointCloudVisualizer.DataAccessLayer;
 
+import android.content.Context;
+import android.util.Log;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+import com.example.jakob.PointCloudVisualizer.DataAccessLayer.CacheNode;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.StringReader;
+import java.nio.ByteBuffer;
+import java.nio.ByteOrder;
 import java.nio.FloatBuffer;
 import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.List;
 
 public class LRUCache {
 
-    static int POINT_COUNT= 32;
+    static int POINT_COUNT= 76;
     static int BLOCK_COUNT= 1024;
     static int FLOAT_SIZE= 4;
     static int BLOCK_SIZE= POINT_COUNT * FLOAT_SIZE * 3;
-    static int BUFFER_SIZE= BLOCK_SIZE * BLOCK_COUNT;
+    static int BUFFER_SIZE= BLOCK_SIZE * (BLOCK_COUNT+1);
+
+    static String SERVER_IP= "http://192.168.2.102:8080/";
+    final int SERVER_PORT = 8080;
+    private final RequestQueue queue;
 
     int capacity;
-    int bufferPointer;
 
     FloatBuffer vertexBuffer;
     FloatBuffer colorBuffer;
 
-    HashMap<Integer, CacheNode> map = new HashMap<>();
+    HashMap<String, CacheNode> map = new HashMap<>();
     CacheNode head=null;
     CacheNode end=null;
 
-    public LRUCache() {
+    public LRUCache(Context context) {
         this.capacity = BLOCK_COUNT;
+        this.queue = Volley.newRequestQueue(context);
+        createFloatBuffers();
         for (int i=0; i < capacity; i++){
-            set(i ,null, i* BLOCK_SIZE);
+            setHead(new CacheNode(Integer.toString(i) + "foo", new float[3], new float[3],
+                    i * POINT_COUNT * 3, vertexBuffer, colorBuffer));
         }
     }
 
-    public void get(int key) {
+    public void get(String key) {
         if(map.containsKey(key)){
             CacheNode n = map.get(key);
             remove(n);
             setHead(n);
         }
-
-        float[] data = null;
-        set(key, data, end.offset);
+        receiveKey(key);
     }
 
-    public void set(int key, float[] data, int offset) {
+    public void set(String key, float[] vertices, float[] colors, int offset) {
         if(map.containsKey(key)) {
             CacheNode old = map.get(key);
             remove(old);
         }
-        CacheNode created = new CacheNode(key, data , offset, vertexBuffer, colorBuffer );
+        CacheNode created = new CacheNode(key, vertices, colors,
+                offset, vertexBuffer, colorBuffer);
         map.remove(end.key);
         remove(end);
         setHead(created);
         map.put(key, created);
+    }
+
+    public void updateCache(String key, float[] vertices, float[] colors){
+        set(key, vertices, colors, end.offset);
     }
 
     private void remove(CacheNode n){
@@ -75,5 +102,56 @@ public class LRUCache {
 
         if(end ==null)
             end = head;
+    }
+
+    private void createFloatBuffers(){
+        ByteBuffer byteBuf = ByteBuffer.allocateDirect(BUFFER_SIZE);
+        byteBuf.order(ByteOrder.nativeOrder());
+        vertexBuffer = byteBuf.asFloatBuffer();
+        vertexBuffer.position(0);
+
+        byteBuf = ByteBuffer.allocateDirect(BUFFER_SIZE);
+        byteBuf.order(ByteOrder.nativeOrder());
+        colorBuffer = byteBuf.asFloatBuffer();
+        colorBuffer.position(0);
+    }
+
+    private void receiveKey(final String key) {
+        final StringRequest sR = new StringRequest(Request.Method.GET, SERVER_IP + "?key=" + key,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        BufferedReader bufReader = new BufferedReader(new StringReader(response));
+                        String line;
+
+                        float[] vertices = new float[3 * POINT_COUNT];
+                        float[] colors= new float[3 * POINT_COUNT];
+                        int i = 0;
+                        int j = 0;
+
+                        try {
+                            while( (line=bufReader.readLine()) != null ) {
+                                String[] responseArray = line.split(" ");
+                                vertices[i++] = Float.parseFloat(responseArray[0]);
+                                vertices[i++] = Float.parseFloat(responseArray[1]);
+                                vertices[i++] = Float.parseFloat(responseArray[2]);
+                                colors[j++] = Float.parseFloat(responseArray[3]);
+                                colors[j++] = Float.parseFloat(responseArray[4]);
+                                colors[j++] = Float.parseFloat(responseArray[5]);
+                            }
+                        } catch (IOException e) {
+                            e.printStackTrace();
+                        }
+                        updateCache(key, vertices, colors);
+                        Log.d("Response", response.substring(0, 500));
+                    }
+                }, new Response.ErrorListener() {
+
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Log.d("Error ", "Http Request didnt work");
+            }
+        });
+        this.queue.add(sR);
     }
 }
