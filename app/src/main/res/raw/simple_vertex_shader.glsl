65d30f5bea799a547f846832383d24ae1c934f73
Jakob Krause <ulukaiz@hotmail.com>
2016-02-22 19:27:40 +0100

    sqrt point size

--- app/src/main/res/raw/simple_vertex_shader.glsl
+++ app/src/main/res/raw/simple_vertex_shader.glsl
@@ -9,5 +9,5 @@ varying vec4 v_Color;
 void main() {
     v_Color = vec4(a_Color, 1.0);
     gl_Position = u_Matrix * (vec4(a_Position, 1.0));
-    gl_PointSize = a_Size;
+    gl_PointSize = sqrt(a_Size);
 }
