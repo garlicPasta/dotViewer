610b2b7e7db46713f04de57d6a9093626ba7e6c7
Jakob Krause <ulukaiz@hotmail.com>
2016-03-14 14:39:52 +0100

    implement drawing Octree

--- app/src/main/java/com/example/jakob/PointCloudVisualizer/GlObjects/OctreeWireGL.java
+++ app/src/main/java/com/example/jakob/PointCloudVisualizer/GlObjects/OctreeWireGL.java
@@ -1,16 +1,47 @@
 package com.example.jakob.PointCloudVisualizer.GlObjects;
 
 
-import java.nio.FloatBuffer;
 
-public class OctreeWireGL {
+import com.example.jakob.PointCloudVisualizer.DataAccessLayer.MultiResTreeProtos;
 
-    private class BoxGL{
-        float center;
+import java.nio.ByteBuffer;
+import java.nio.ShortBuffer;
+
+import static android.opengl.GLES20.GL_UNSIGNED_SHORT;
+import static android.opengl.GLES20.glDrawElements;
+import static android.opengl.GLES20.GL_LINE_STRIP;
+import static com.example.jakob.PointCloudVisualizer.util.BufferHelper.buildShortBuffer;
+
+public class OctreeWireGL{
+
+    private class OctreeNodeGL {
+        OctreeWireGL[] octants = new OctreeWireGL[8];
+        float[] center;
         float length;
-        FloatBuffer verticeBuffer;
-        FloatBuffer indices;
-        float[][] vertices;
+        BoxGL box;
+
+        public OctreeNodeGL(MultiResTreeProtos.MRTree.MRNode node) {
+            super();
+            center = new float[]{(float) node.getCenter(0),
+                    (float) node.getCenter(1),
+                    (float) node.getCenter(2)};
+            length = (float) node.getCellLength();
+            box = new BoxGL(center, length);
+        }
+
+        public void draw(){
+            box.draw();
+            for (int i = 0; i < octants.length; i++) {
+                if (octants[i] != null)
+                    octants[i].draw();
+            }
+        }
+    }
+
+    private class BoxGL extends ModelGL{
+
+        protected ShortBuffer mIndexBuffer;
+
         float[] cubeVertices = new float[]{
                 -1.0f, -1.0f, 1.0f,
                 1.0f, -1.0f, 1.0f,
@@ -38,25 +69,30 @@ public class OctreeWireGL {
 
 
         BoxGL(float[] center,float length){
+            super(ByteBuffer.allocateDirect(24 * 4).asFloatBuffer());
             for (int i = 0; i < cubeVertices.length / 3 ; i++) {
                 for (int j = 0; j < 3; j++) {
                     cubeVertices[3 * i + j] *= length;
                     cubeVertices[3 * i + j] += center[j];
                 }
             }
+            this.mVertexBuffer.put(cubeVertices);
+            this.mIndexBuffer = buildShortBuffer(indices);
         }
 
-        private void draw(){
+        @Override
+        public void draw(){
+            glDrawElements(GL_LINE_STRIP, 12, GL_UNSIGNED_SHORT, mIndexBuffer);
         }
     }
 
-    private BoxGL root;
+    private OctreeNodeGL root;
 
-    OctreeWireGL(){
-
+    OctreeWireGL(MultiResTreeProtos.MRTree tree){
+        root = new OctreeNodeGL(tree.getRoot());
     }
 
     public void draw(){
-
+        root.draw();
     }
 }
--- app/src/main/java/com/example/jakob/PointCloudVisualizer/util/BufferHelper.java
+++ app/src/main/java/com/example/jakob/PointCloudVisualizer/util/BufferHelper.java
@@ -3,6 +3,7 @@ package com.example.jakob.PointCloudVisualizer.util;
 import java.nio.ByteBuffer;
 import java.nio.ByteOrder;
 import java.nio.FloatBuffer;
+import java.nio.ShortBuffer;
 
 public class BufferHelper {
 
@@ -10,6 +11,15 @@ public class BufferHelper {
         ByteBuffer byteBuf = ByteBuffer.allocateDirect(array.length * typeSize);
         byteBuf.order(ByteOrder.nativeOrder());
         FloatBuffer buffer = byteBuf.asFloatBuffer();
+        buffer.put(array);
+        buffer.position(0);
+        return buffer;
+    }
+
+    public static ShortBuffer buildShortBuffer(short[] array) {
+        ByteBuffer byteBuf = ByteBuffer.allocateDirect(array.length * 2);
+        byteBuf.order(ByteOrder.nativeOrder());
+        ShortBuffer buffer = byteBuf.asShortBuffer();
         buffer.put(array);
         buffer.position(0);
         return buffer;
